2cae3f7f9828f1abae37626c11c02277230aad30
osexcel <osexcel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-10 23:33:39 +0000

    Remove Secret Word Description Field
    git-svn-id: https://plugins.svn.wordpress.org/ose-firewall/tags/2.0.1@769998 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- public/js/scanconfig.js
+++ public/js/scanconfig.js
@@ -16,7 +16,7 @@ oseConfScan.Form = new Ext.FormPanel({
         height: 350,
         renderTo: 'ConfigScan',
         items: [
-        		oseGetDisplayField(O_SECRET_WORD_DESC),
+        		//oseGetDisplayField(O_SECRET_WORD_DESC),
         		//oseGetNormalTextField ('secretword', O_SECRET_WORD, 320, 600),
         		oseGetDisplayField(O_ANTI_HACKING_SCANNING_OPTIONS),
 				oseGetCombo('devMode', O_DEVELOPMENT_MODE, oseConfScan.Option, 600, 320, 100, 0),
