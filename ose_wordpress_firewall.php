76c15b1183ce27d580b5edb573124dd84322ea0e
osexcel <osexcel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-13 04:36:51 +0000

    Update Author Information
    git-svn-id: https://plugins.svn.wordpress.org/ose-firewall/tags/2.0.2@771374 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ose_wordpress_firewall.php
+++ ose_wordpress_firewall.php
@@ -2,10 +2,10 @@
 /*
 Plugin Name: OSE Firewall
 Plugin URI: http://wordpress.org/extend/plugins/ose-firewall/
-Description: OSE Firewall - A WordPress Firewall created by Open Source Excellence. It protects your WordPress-powered blog against attacks and hacking. The email alert / notification function is disabled by default, while it can be activated and configured in <strong>Settings -> OSE Firewall</strong>. Please go to your <a href="admin.php?page=ose_wp_firewall">OSE Firewall configuration</a> page.
-Author: Open Sourcce Excellence
+Description: OSE Firewall - A WordPress Firewall created by ProWeb (Protect Website). It protects your WordPress-powered blog against attacks and hacking. The email alert / notification function is disabled by default, while it can be activated and configured in <strong>Settings -> OSE Firewall</strong>. Please go to your <a href="admin.php?page=ose_wp_firewall">OSE Firewall configuration</a> page.
+Author: ProWeb (Protect Website)
 Version: 2.0.2
-Author URI: http://www.opensource-excellence.com/
+Author URI: http://www.protect-website.com/
 */
 
 // Basic configuration; 
