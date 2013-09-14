60e8c58731a41b4ca652feb6843b31f7335f948c
osexcel <osexcel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-14 06:31:41 +0000

    Clear version 2.0.2
    git-svn-id: https://plugins.svn.wordpress.org/ose-firewall/trunk@772083 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ose_firewall_badge.php
+++
@@ -1,9 +0,0 @@
-<?php  
-/* 
-   Plugin Name: OSE Firewall Badge
-   Description: Plugin For Showing OSE Firewall Badge 
-   Author: Protect Website
-   Version: 2.0.2
-*/  
-include('includes/oseBadgeWidget.php');
-?>
