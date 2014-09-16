4af854a0d8664a383333ce6227194cc0a0ede9a4
osexcel <osexcel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-16 04:25:23 +0000

    = 3.8.4 = * Fixed: Fixed incorrect database export download link issue
    git-svn-id: https://plugins.svn.wordpress.org/ose-firewall/trunk@990771 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- protected/config/wordpress.php
+++ protected/config/wordpress.php
@@ -46,5 +46,5 @@ define('OSE_FWDATA', OSEFWDIR . 'protected' . ODS.'data');
 define('OSE_DEFAULT_SCANPATH', ABSPATH);
 define('DB_BACKUP_DOWNLOAD_URL', OSE_WPURL . '/wp-admin/admin-ajax.php?option=ose_firewall&task=downloadBackupDB&action=downloadBackupDB&controller=backup&ids=');
 define('FILE_BACKUP_DOWNLOAD_URL', OSE_WPURL . '/wp-admin/admin-ajax.php?option=ose_firewall&task=downloadBackupFile&action=downloadBackupFile&controller=backup&ids=');
-define('EXPORT_DOWNLOAD_URL', OSE_WPURL . '/wp-admin/admin-ajax.php?option=ose_firewall&task=exportCSV&action=downloadCSV&controller=manageips&filename=');
+define('EXPORT_DOWNLOAD_URL', OSE_WPURL . '/wp-admin/admin-ajax.php?option=ose_firewall&task=downloadCSV&action=downloadCSV&controller=manageips&filename=');
 ?>
