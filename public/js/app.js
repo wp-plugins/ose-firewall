c08f13a4c85d4ed2a9c46df83c19137d8ec71815
osexcel <osexcel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-18 00:55:37 +0000

    = 4.0.3 = * Enhancement: Improved scanning class to harden protection and avoid IP spoofing * Enhancement: Improved dashboard section to avoid CSRF attack * Fixed: Fixed error warning for WordPress website with lower version
    git-svn-id: https://plugins.svn.wordpress.org/ose-firewall/tags/4.0.3@1027631 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- public/js/app.js
+++ public/js/app.js
@@ -314,10 +314,12 @@ jQuery(document).ready(function($){
 jQuery(document).ready(function($){
 	$("#configuraton-form").submit(function() {
 		showLoading();
+		var data = $("#configuraton-form").serialize();
+		data += '&centnounce='+$('#centnounce').val();
         $.ajax({
                type: "POST",
                url: url,
-               data: $("#configuraton-form").serialize(), // serializes the form's elements.
+               data: data, // serializes the form's elements.
                success: function(data)
                {
             	   data = jQuery.parseJSON(data);
@@ -337,10 +339,12 @@ jQuery(document).ready(function($){
 	
 	$("#seo-configuraton-form").submit(function() {
 		showLoading();
+		var data = $("#seo-configuraton-form").serialize();
+		data += '&centnounce='+$('#centnounce').val();
         $.ajax({
                type: "POST",
                url: url,
-               data: $("#seo-configuraton-form").serialize(), // serializes the form's elements.
+               data: data, // serializes the form's elements.
                success: function(data)
                {
             	   data = jQuery.parseJSON(data);
@@ -360,10 +364,12 @@ jQuery(document).ready(function($){
 	
 	$("#admin-configuraton-form").submit(function() {
 		showLoading();
+		var data = $("#admin-configuraton-form").serialize();
+		data += '&centnounce='+$('#centnounce').val();
         $.ajax({
                type: "POST",
                url: url,
-               data: $("#admin-configuraton-form").serialize(), // serializes the form's elements.
+               data: data, // serializes the form's elements.
                success: function(data)
                {
             	   data = jQuery.parseJSON(data);
@@ -386,10 +392,12 @@ jQuery(document).ready(function($){
 	
 	$("#scan-form").submit(function() {
 		showLoading();
+		var data = $("#scan-form").serialize();
+		data += '&centnounce='+$('#centnounce').val();
         $.ajax({
                type: "POST",
                url: url,
-               data: $("#scan-form").serialize(), // serializes the form's elements.
+               data: data, // serializes the form's elements.
                success: function(data)
                {
             	   hideLoading();
