c4548d4e1651f4b29ef61d36a68653b33203cc64
osexcel <osexcel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-20 23:03:03 +0000

    Add back subscribe button in login page
    git-svn-id: https://plugins.svn.wordpress.org/ose-firewall/trunk@1164556 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/views/login.php
+++ assets/views/login.php
@@ -42,6 +42,7 @@ $urls = oseFirewall::getDashboardURLs ();
 								<h3 class="panel-title"><?php oLang::_('SUBSCRIPTION ACTIVATION'); ?></h3>
 							</div>
 							<div class="panel-controls-buttons">
+								 <button onclick="redirectTut('http://www.centrora.com/store/subscription-packages/');" type="button" class="btn btn-primary btn-sm mr5 mb10">Subscribe To A Plan Now</button>
 								 <button onclick="redirectTut('http://www.centrora.com/store/index.php?route=affiliate/login');" type="button" class="btn btn-yellow btn-sm mr5 mb10">Get Your Premium Service For FREE</button>
 	                             <button class="btn btn-danger btn-sm mr5 mb10" type="button" onClick="redirectTut('https://www.centrora.com/store/activating-premium-service');"><?php oLang::_('TUTORIAL'); ?></button>
 	                        </div>
