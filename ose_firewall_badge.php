63e5e87c44d73e543836e31ff2d5d9d7880d4319
osexcel <osexcel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-10 01:11:13 +0000

    = 4.9.5 = * Fixed: fixed a bug in virus scanner to detect PHP injection scripts
    git-svn-id: https://plugins.svn.wordpress.org/ose-firewall/trunk@1195939 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ose_firewall_badge.php
+++ ose_firewall_badge.php
@@ -3,7 +3,7 @@
    Plugin Name: Centrora Security Badge
    Description: Plugin For Showing Centrora Security Badge 
    Author: Centrora Security
-   Version: 4.9.4
+   Version: 4.9.5
 */  
 include(dirname(__FILE__).'/includes/oseBadgeWidget.php');
 ?>
